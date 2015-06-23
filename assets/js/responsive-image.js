19226ab0e9fc1005cf8e3cb2a7c5cf523df6e95e
Niels Braczek <nbraczek@bsds.de>
2015-06-23 17:27:42 +0200

    Removed debug code

--- assets/js/responsive-image.js
+++ assets/js/responsive-image.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    alert("Processing feature image");
     var cWidth = window.innerWidth;
-    alert("Canvas width is " + cWidth);
     var width = 1920;
     var sizes = [480, 768, 1024, 1280, 1920];
     for (var i = 0; i < sizes.length; ++i) {
@@ -10,10 +8,8 @@ $(document).ready(function () {
             break;
         }
     }
-    alert("Best fit is " + width);
 
     var $img = $('img#feature-image');
-    alert("Found feature image " + $img.data('ref'));
     $img.attr({
         'src': "/images/" + width + "/" + $img.data('ref')
     });
